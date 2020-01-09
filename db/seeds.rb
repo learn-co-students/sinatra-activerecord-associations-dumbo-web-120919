9f90b0193932895e8aca4bcc9a92deb2f05593eb
Summer Rizzo <srainrizzo@gmail.com>
2020-01-09 11:33:04 -0500

    Done.

---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+Cat.create(:name => "Maru", :age => 3, :breed => "Scottish Fold")
+Cat.create(:name => "Felix", :age => 2, :breed => "Tuxedo")
+Cat.create(:name => "Patches", :age => 2, :breed => "Calico")
+
+Owner.create(:name => "Sums")
+Owner.create(:name => "Chris")
